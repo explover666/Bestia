ff5f40d03f507a461fe23d5765e92ec6f8367d54
Explov666 <explover666@gmail.com>
2016-11-05 13:52:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/max/bestia/Fragments/PageFragment.java
@@ -0,0 +1,43 @@
+package com.example.max.bestia.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.max.bestia.Database.DatabaseHelper;
+import com.example.max.bestia.R;
+
+public class PageFragment extends Fragment {
+
+    static final String ARGUMENT_PAGE_NUMBER = "arg_page_number";
+
+    int pageNumber;
+    private DatabaseHelper dbHelper;
+
+    public static PageFragment newInstance(int page) {
+        PageFragment pageFragment = new PageFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(ARGUMENT_PAGE_NUMBER, page);
+        pageFragment.setArguments(arguments);
+        return pageFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageNumber = getArguments().getInt(ARGUMENT_PAGE_NUMBER);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.pager_item, null);
+        ImageView poster = (ImageView) view.findViewById(R.id.imageView);
+        dbHelper = new DatabaseHelper(getContext());
+        poster.setImageBitmap(dbHelper.PutImage(pageNumber + 1));
+        return view;
+    }
+}
